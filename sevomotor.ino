d1215d606e9614b1f9b105d9d1b7ce68579a3785
palakraj50 <127666762+palakraj50@users.noreply.github.com>
2023-03-12 18:04:52 +0530

    Railway Accidents Prevention
    To automatic  platform will be extend when train come.

---
+++ sevomotor.ino
@@ -0,0 +1,63 @@
+#include <Servo.h>
+
+Servo servo1;
+
+int trigPin = 9;
+
+int echoPin = 8;
+
+long distance;
+
+long duration;
+
+void setup() 
+
+{
+
+servo1.attach(7); 
+
+ pinMode(trigPin, OUTPUT);
+
+ pinMode(echoPin, INPUT);// put your setup code here, to run once:
+
+}
+
+void loop()
+
+{
+
+  ultra_sonic();
+
+  servo1.write(90);
+
+  if(distance <=9)
+
+  {
+
+  servo1.write(270);
+
+  }
+
+}
+
+void ultra_sonic()
+
+{
+
+  digitalWrite(trigPin, LOW);
+
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration*0.034/2;
+
+  }
+
+0
